7aef51baf2270de8d4722ea9bfe6c631d395d52b
lance.enario <lancejoseph.enario@gmail.com>
2024-09-20 00:35:17 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "linkedlist.h"
+using namespace std;
+
+int main (){
+    LinkedList* list = new LinkedList();
+    list->add(1);
+    list->add(2);
+    list->add(3); 
+    list->add(5);
+    list->add(4);
+    list->print();
+}
